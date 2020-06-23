bf2316d0499c17f748634081d7a04e49df8311b5
Bomi <a51022866@gmail.com>
2020-06-23 21:43:07 +0900

    Feat : Dollar class 생성 #2
    컴파일 에러를 해결하기 위한 클래스 생성
    * 테스트 케이스는 통과 실패

---
+++ src/example/money/Dollar.java
@@ -0,0 +1,19 @@
+package example.money;
+
+/**
+ * @author Bomi
+ * @date 2020/06/23
+ */
+
+public class Dollar {
+    int amount;
+
+    Dollar(int amount) {
+
+    }
+
+    void times(int multiplier) {
+
+    }
+
+}

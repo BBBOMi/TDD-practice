ba084c30dc58b2800163a80bb1803ecaab70e4f6
Bomi <a51022866@gmail.com>
2020-06-23 22:05:49 +0900

    Refactor : 생성자와 메서드 수정 #2
    변경을 통해 테스트 코드와 코드 사이의 중복을 걷어냄

--- src/example/money/Dollar.java
+++ src/example/money/Dollar.java
@@ -9,10 +9,11 @@ public class Dollar {
     int amount;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-        amount = 5 * 2;
+        amount *= 2;
     }
 
 }

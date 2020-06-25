8ad0d95183b07068f379183700a42b9f4a1cbdee
Bomi <a51022866@gmail.com>
2020-06-25 23:08:16 +0900

    Feat : 화폐 클래스의 공통된 필드와 메서드 추가 #13
    - Dollar와 Franc에서 공통된 필드 amount와 메서드 equals()를 추가

--- src/example/money/Money.java
+++ src/example/money/Money.java
@@ -6,4 +6,11 @@ package example.money;
  */
 
 public class Money {
+    protected int amount;
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return this.amount == money.amount;
+    }
 }

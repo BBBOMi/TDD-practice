6d7f01a966164d6e932377874921eb6ff72b9b6f
Bomi <a51022866@gmail.com>
2020-08-20 17:32:44 +0900

    Refactor : FrancTest class 제거 #26
    - FrancTest class의 메서드를 DollarTest class로 이동
    - 중복되거나 불필요한 테스트 메서드 제거
    - 테스트 검증 완료

--- test/example/money/DollarTest.java
+++ test/example/money/DollarTest.java
@@ -12,20 +12,29 @@ import static org.junit.Assert.*;
 public class DollarTest {
 
     @Test
-    public void testMultiplication2() {
+    public void testDollarMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
+    public void testFrancMultiplication() {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     @Test
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
     }
 }

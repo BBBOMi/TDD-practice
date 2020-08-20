43d60012ce7ebc56aa9e688cea67e409a05d6b46
Bomi <a51022866@gmail.com>
2020-08-20 18:58:00 +0900

    Feat : Bank 클래스 생성 #28
    - 통화 단위를 변경할 수 있는  stub 메서드 구현
    - 테스트 검증 확인

---
+++ src/example/money/Bank.java
@@ -0,0 +1,12 @@
+package example.money;
+
+/**
+ * @author Bomi
+ * @date 2020/08/20
+ */
+
+public class Bank {
+    Money reduce(Expression source, String to) {
+        return Money.dollar(10);
+    }
+}

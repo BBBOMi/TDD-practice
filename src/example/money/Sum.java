a2321df096037f71412036976b4cc0f1b814277f
Bomi <a51022866@gmail.com>
2020-09-02 15:30:44 +0900

    Refactor : Expression을 구현하게 변경 #28
    - Sum도 Expression의 하나이므로 Expression을 구현하게 함
    - Money의 plus 메서드 변경에 따라 생성자 생성
    - 테스트 통과 확인

--- src/example/money/Sum.java
+++ src/example/money/Sum.java
@@ -5,7 +5,12 @@ package example.money;
  * @date 2020/09/02
  */
 
-public class Sum {
+public class Sum implements Expression {
     Money augend;
     Money addend;
+
+    Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
 }

b9692916e760822ba7e06952e31d38b57560f065
Bomi <a51022866@gmail.com>
2020-09-02 16:10:07 +0900

    Refactor : reduce 로직 수정 #28
    - 두 단계에 걸쳐 접근해 연산 -> Sum에게 역할을 넘김

--- src/example/money/Bank.java
+++ src/example/money/Bank.java
@@ -8,7 +8,6 @@ package example.money;
 public class Bank {
     Money reduce(Expression source, String to) {
         Sum sum = (Sum) source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }

7a2609700e7ae5fe4cf9f44a347ac0c5eefa9f87
Bomi <a51022866@gmail.com>
2020-09-02 16:43:39 +0900

    Refactor : 불필요한 코드 제거 #29
    - Expression의 구현체는 reduce()를 구현하고 있으므로, 클래스를 명시적으로 검사하지 않아도 됨.
    - 테스트 통과 확인

--- src/example/money/Bank.java
+++ src/example/money/Bank.java
@@ -7,9 +7,6 @@ package example.money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        if(source instanceof Money)
-            return (Money)source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }

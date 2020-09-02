badcd49b8906a06bb98ae46618bd37fa2e839857
Bomi <a51022866@gmail.com>
2020-09-02 16:38:21 +0900

    Refactor : Money가 reduce를 구현하게 함 #29
    - 아래의 Sum과 메서드 시그니처가 동일하게 작성
    - 컴파일 에러 발생

--- src/example/money/Bank.java
+++ src/example/money/Bank.java
@@ -7,7 +7,8 @@ package example.money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        if(source instanceof Money) return (Money)source;
+        if(source instanceof Money)
+            return (Money)source.reduce(to);
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }

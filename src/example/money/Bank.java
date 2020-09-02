e7471dd7bf20547b1b4a7c7036be844df990d3de
Bomi <a51022866@gmail.com>
2020-09-02 16:34:20 +0900

    Refactor : Money가 들어오면 그대로 리턴하게 변경 #29
    - 테스트 코드 통과 확인
    - 다형성을 이용해 리팩토링 할 예정

--- src/example/money/Bank.java
+++ src/example/money/Bank.java
@@ -7,6 +7,7 @@ package example.money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
+        if(source instanceof Money) return (Money)source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }

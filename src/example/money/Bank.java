f5900533ed946779335a90823c843ad3475c1b3c
Bomi <a51022866@gmail.com>
2020-10-03 22:34:10 +0900

    Feat : 환율 계산 메서드 생성 #31
    - 환율을 Bank에서 계산할 수 있도록  함

--- src/example/money/Bank.java
+++ src/example/money/Bank.java
@@ -9,4 +9,8 @@ public class Bank {
     Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") &&to.equals("USD")) ? 2 : 1;
+    }
 }

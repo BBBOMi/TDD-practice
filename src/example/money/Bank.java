3377c47d0e3587d11c98f912a501dc877e30f6c7
Bomi <a51022866@gmail.com>
2020-10-03 22:57:41 +0900

    Feat : 통화를 짝지어 관리할 클래스 생성 #31
    - 환율은 Bank 클래스에서 계산하므로, 내부에 private class로 생성
    -  임시로 hashcode 정의

--- src/example/money/Bank.java
+++ src/example/money/Bank.java
@@ -13,4 +13,25 @@ public class Bank {
     int rate(String from, String to) {
         return (from.equals("CHF") &&to.equals("USD")) ? 2 : 1;
     }
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            Pair pair = (Pair) object;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
 }
